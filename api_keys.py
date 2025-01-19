c2f2c082eeaed8420be316e3821ccc131cddc719
Dkelsey11 <dfkelsey07@gmail.com>
2025-01-19 06:27:58 -0600

    hw6

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "75cda6add51c9860c15d528ad92ad64b"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "095c2d2edf2d4044a7536fbdfd28a44b"
